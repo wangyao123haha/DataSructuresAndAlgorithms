df81d18ed20d9f6cca5a9ec484db4f3b244588ed
wangyao123haha <412923188@qq.com>
2024-03-17 23:16:47 +0800

    约瑟夫算法

---
+++ src/suanfa/stack/ArrayStackDemo.java
@@ -0,0 +1,56 @@
+package suanfa.stack;
+
+/**
+ * @Description 数组模拟栈
+ * @Author wangyao
+ * @Data 2024/3/17 23:03
+ */
+
+public class ArrayStackDemo {
+
+    public static void main(String[] args) {
+        ArrayStack arrayStack = new ArrayStack(10);
+    }
+}
+class ArrayStack{
+
+    private int maxSize;//栈最大深度
+    private int[] stack;
+    private int top = -1;
+
+    public ArrayStack(int size){
+        this.maxSize = size;
+        stack = new int[this.maxSize];
+    }
+    public boolean isFull(){
+        return top == maxSize-1;
+    }
+    public boolean isEmpty(){
+        return top == -1;
+    }
+    public void push(int value){
+        if(isFull()){
+            System.out.println("栈满");
+            return;
+        }
+        top++;
+        stack[top] = value;
+    }
+    public int pop(){
+        if(isEmpty()){
+            throw new RuntimeException("栈是空的");
+        }
+        int value = stack[top];
+        top--;
+        return value;
+    }
+    public void showStack(){
+        if(isEmpty()){
+            System.out.println("栈是空的");
+        }
+        for(int i = top;i>=0;i--){
+            System.out.printf("stack[%d]=%d\n",i,stack[i]);
+        }
+    }
+}
+
