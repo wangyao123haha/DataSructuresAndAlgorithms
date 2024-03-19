5b154f317d6dedd60f7e7370f381993e98ef22ec
wangyao123haha <412923188@qq.com>
2024-03-19 23:13:37 +0800

    递归迷宫

--- src/suanfa/recursion/MiGong.java
+++ src/suanfa/recursion/MiGong.java
@@ -8,6 +8,65 @@ package suanfa.recursion;
 
 public class MiGong {
     public static void main(String[] args) {
-
+        // 二维数组模拟迷宫
+        int [][] map = new int[8][7];
+        for(int i =0;i<7;i++){
+            map[0][i] = 1;
+            map[7][i] = 1;
+        }
+        for(int i= 0;i<8;i++){
+            map[i][0] = 1;
+            map[i][6] = 1;
+        }
+        map[3][1] =1;
+        map[3][2] =1; 
+        System.out.println("当前地图~~~~");
+        for(int i=0;i<8;i++){
+            for(int j =0;j<7;j++){
+                System.out.print(map[i][j]+" ");
+            }
+            System.out.println();
+        }
+        // 模拟迷宫
+        setWay(map,1,1);
+        System.out.println("迷宫走完~~~~");
+        for(int i=0;i<8;i++){
+            for(int j =0;j<7;j++){
+                System.out.print(map[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+    /**
+     * 1 表示墙  2 表示可以走 3表示已经走过
+     */
+    public static boolean setWay(int[][] map,int i,int j){
+        if(map[6][5]==2){
+            // 已经找到出路
+            return true;
+        }else{
+            // 当前位置是0 可以继续走
+            if(map[i][j]==0){
+                // 制定策略 //上下左右走
+                map[i][j] =2;
+                if(setWay(map,i+1,j)){
+                    // 向下走
+                    return true;
+                } else if (setWay(map,i,j+1)) {
+                    // 向右走
+                    return true;
+                }else if(setWay(map,i-1,j)){
+                    // 向上走
+                    return true;
+                }else if(setWay(map,i,j-1)){
+                    return true;
+                }else{
+                    map[i][j] = 3;
+                    return false;
+                }
+            }else{
+                return false;
+            }
+        }
     }
 }
