e69003d8f7bbf3210b84f358974766603cd2bd92
wangyao123haha <412923188@qq.com>
2024-03-20 22:09:43 +0800

    递归迷宫

--- src/suanfa/recursion/MiGong.java
+++ src/suanfa/recursion/MiGong.java
@@ -1,5 +1,8 @@
 package suanfa.recursion;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @Description 迷宫
  * @Author wangyao
@@ -27,8 +30,10 @@ public class MiGong {
             }
             System.out.println();
         }
+        List<Integer> path = new ArrayList<>();
         // 模拟迷宫
-        setWay(map,1,1);
+        setWay(map,1,1,path);
+        System.out.println(path.size());
         System.out.println("迷宫走完~~~~");
         for(int i=0;i<8;i++){
             for(int j =0;j<7;j++){
@@ -39,8 +44,9 @@ public class MiGong {
     }
     /**
      * 1 表示墙  2 表示可以走 3表示已经走过
+     * 求出最短路径
      */
-    public static boolean setWay(int[][] map,int i,int j){
+    public static boolean setWay(int[][] map, int i, int j, List<Integer> path){
         if(map[6][5]==2){
             // 已经找到出路
             return true;
@@ -49,16 +55,21 @@ public class MiGong {
             if(map[i][j]==0){
                 // 制定策略 //上下左右走
                 map[i][j] =2;
-                if(setWay(map,i+1,j)){
+                if(setWay(map,i+1,j,path)){
+                    path.add(map[i][j]);
                     // 向下走
                     return true;
-                } else if (setWay(map,i,j+1)) {
+                } else if (setWay(map,i,j+1,path)) {
+                    path.add(map[i][j]);
                     // 向右走
                     return true;
-                }else if(setWay(map,i-1,j)){
+                }else if(setWay(map,i-1,j,path)){
+                    path.add(map[i][j]);
                     // 向上走
                     return true;
-                }else if(setWay(map,i,j-1)){
+                }else if(setWay(map,i,j-1,path)){
+                    path.add(map[i][j]);
+                    // 向左走
                     return true;
                 }else{
                     map[i][j] = 3;
